c153955722ffc01aea7a8af108202869c34d941b
ramandeep28 <ramandeep.singh28jan@gmail.com>
2023-04-05 11:53:57 +0530

    update

--- Projects/Spotify Clone/script.js
+++ Projects/Spotify Clone/script.js
@@ -9,17 +9,15 @@ let myProgressBar = document.getElementById("myProgressBar");
 let gif = document.getElementById("gif");
 
 let songs = [
-  { songName: "420", filePath: "songs/1.mp3", coverPath: "pics/covers/1.jpg" },
-  { songName: "420", filePath: "songs/1.mp3", coverPath: "pics/covers/1.jpg" },
-  { songName: "420", filePath: "songs/1.mp3", coverPath: "pics/covers/1.jpg" },
-  { songName: "420", filePath: "songs/1.mp3", coverPath: "pics/covers/1.jpg" },
-  { songName: "420", filePath: "songs/1.mp3", coverPath: "pics/covers/1.jpg" },
-  { songName: "420", filePath: "songs/1.mp3", coverPath: "pics/covers/1.jpg" },
-  { songName: "420", filePath: "songs/1.mp3", coverPath: "pics/covers/1.jpg" },
-  { songName: "420", filePath: "songs/1.mp3", coverPath: "pics/covers/1.jpg" },
+  { songName: "Mi-Amor", filePath: "songs/1.mp3", coverPath: "pics/covers/1.jpg" },
+  { songName: "420", filePath: "songs/2.mp3", coverPath: "pics/covers/2.jpg" },
+  { songName: "420", filePath: "songs/3.mp3", coverPath: "pics/covers/3.jpg" },
+  { songName: "420", filePath: "songs/4.mp3", coverPath: "pics/covers/4.jpg" },
+  { songName: "420", filePath: "songs/5.mp3", coverPath: "pics/covers/5.jpg" },
+  { songName: "420", filePath: "songs/6.mp3", coverPath: "pics/covers/6.jpg" },
+  { songName: "420", filePath: "songs/7.mp3", coverPath: "pics/covers/7.jpg" },
+  { songName: "420", filePath: "songs/8.mp3", coverPath: "pics/covers/8.jpg" },
 ];
-
-// audioElement.play();
 
 //handle play/pause click
 masterPlay.addEventListener("click", () => {
@@ -38,14 +36,17 @@ masterPlay.addEventListener("click", () => {
 
 //listen_to_events
 audioElement.addEventListener("timeupdate", () => {
-  //   console.log("timeupdate");
+  console.log("timeupdate");
   //update seekbar
   progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
-  //   console.log(progress);
+  // console.log("currtime", audioElement.currentTime);
+  // console.log("duration", audioElement.duration);
+  // // this will give progress of the song that is being played.
+  // console.log(progress);
   myProgressBar.value = progress;
 });
 
+// when we seek the progress bar, we seek the audio to that time also. 
 myProgressBar.addEventListener("change", () => {
-  audioElement.currentTime =
-    (myProgressBar.value * audioElement.duration) / 100;
+  audioElement.currentTime =(myProgressBar.value * audioElement.duration) / 100;
 });
